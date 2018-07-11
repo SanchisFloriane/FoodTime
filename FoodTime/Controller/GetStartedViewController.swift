73773dbcb79da322ebcfe17592dde6d1e8ef684d
sanchis floriane <sanchis.floriane@live.fr>
2018-07-11 15:00:51 -0700

    - Creation of the get started page and login page (the login page is not functional) = design

---
+++ FoodTime/Controller/GetStartedViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GetStartedViewController.swift
+//  FoodTime
+//
+//  Created by bob on 7/11/18.
+//  Copyright © 2018 sanchisfloriane. All rights reserved.
+//
+
+import UIKit
+
+class GetStartedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
